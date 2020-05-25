0a2c8e7001740df9b3bace5644fa0901863ff25e
“RUI” <278350644@qq.com>
2020-05-25 12:29:19 +1000

    deal duplications

--- R/1_process_raw.R
+++ R/1_process_raw.R
@@ -1,47 +1,99 @@
-install.packages("ggplot2", "tidyvers")
-library(tidyverse,ggplot2)
+library(tidyverse) 
+library(dplyr)
+library(ggplot2)
+library(broom)
+library(readxl)
+library(stringi)
+library(knitr)
 
-# read and save all data in R
+# Read and save all data in R
 p_st <- read_csv("data/raw/2018-19_nba_player_statistics.csv")
 p_sal <- read_csv("data/raw/2018-19_nba_player-salaries.csv")
 t_st1 <- read_csv("data/raw/2018-19_nba_team_statistics_1.csv")
 t_st2 <- read_csv("data/raw/2018-19_nba_team_statistics_2.csv")
 payroll <-read_csv("data/raw/2019-20_nba_team-payroll.csv")
 
-#rename all variables which contain %
-p_st <- rename(p_st, FGp = `FG%`, x3P = `3P`, x3PA = `3PA`, x3Pp = `3P%`, x2P = `2P`, 
-               x2PA = `2PA`, x2Pp = `2P%`, eFGp = `eFG%`, FTp = `FT%`)
+#Check the Data
+head(p_st)
+tail(p_st)
+str(p_st)
+
+head(p_sal)
+tail(p_sal)
+str(p_sal)
+
+head(t_st1)
+tail(t_st1)
+str(t_st1)
+
+head(t_st2)
+tail(t_st2)
+str(t_st2)
+
+head(payroll)
+tail(payroll)
+str(payroll)
+
+# Data Transformation
+
+#1- Rename all variables which contain symbols
+p_st <- rename(p_st, FGp = 'FG%', x3P = '3P', x3PA = '3PA', x3Pp = '3P%', x2P = '2P', 
+               x2PA = '2PA', x2Pp = '2P%', eFGp = 'eFG%', FTp = 'FT%')
 t_st1 <- rename(t_st1, x3PAr = "3PAr", TSp= "TS%" , eFGp = "eFG%", TOVp = "TOV%",
                 ORBp = "ORB%", FT_FGA = "FT/FGA", DRBp = "DRB%" )
-t_st2 <- rename(t_st2, FGp = `FG%`, x3P = `3P`, x3PA = `3PA`, x3Pp = `3P%`, x2P = `2P`, 
-                x2PA = `2PA`, x2Pp = `2P%`, FTp = `FT%`)
+t_st2 <- rename(t_st2, FGp ='FG%', x3P = '3P', x3PA = '3PA', x3Pp = '3P%', x2P = '2P', 
+                x2PA = '2PA', x2Pp = '2P%', FTp = 'FT%')
 
-#delet missing value in player salaries(p_sal)
-which(is.na(p_sal), arr.ind = TRUE)
-p_sal<- select(p_sal, player_id, player_name, salary)
-sum(is.na(p_sal))
+#2- Replace and delete unrecognized symbols
 
-
-# delet missing value in team statistics 1(t_st1)
-which(is.na(t_st1), arr.ind = TRUE)
-t_st1<- select(t_st1, -c(X23:25))
-sum(is.na(t_st1))
-
-
-#deal missing values in player statistics (p_st)
-sum(is.na(p_st))
-which(is.na(p_st), arr.ind = TRUE)
-naniar::vis_miss(p_st)
-p_st <- replace_na(p_st,list(FGp = "0", x3Pp = "0", x2Pp = "0", eFGp = "0", FTp = "0" ))
-sum(is.na(p_st))
-
-# change "salary"'s class (payroll)
+# Change "salary"'s class (payroll)
 payroll$salary <- str_remove_all(payroll$salary, "\\$")
 payroll$salary <- str_remove_all(payroll$salary, "\\,")
 payroll$salary <- str_remove_all(payroll$salary, "\\,")
 payroll$salary <- as.numeric(payroll$salary)
 
+# Adjust players' name
+p_st$player_name <- p_st$player_name %>%
+  stringi::stri_trans_general("Latin-ASCII") %>%
+  stringr::str_replace_all(pattern = "\\.", replacement = "")
 
+# 3- Dealing missing value
+
+sum(is.na(p_sal))
+sum(is.na(p_st))
+sum(is.na(payroll))
+sum(is.na(t_st1))
+sum(is.na(t_st2))
+
+#Delet missing value in player salaries(p_sal)
+which(is.na(p_sal), arr.ind = TRUE)
+p_sal<- select(p_sal, player_id, player_name, salary)
+sum(is.na(p_sal))
+
+# Delet missing value in team statistics 1(t_st1)
+which(is.na(t_st1), arr.ind = TRUE)
+t_st1<- select(t_st1, -c(X23:25))
+sum(is.na(t_st1))
+
+#Deal missing values in player statistics (p_st)
+sum(is.na(p_st))
+which(is.na(p_st), arr.ind = TRUE)
+naniar::vis_miss(p_st)
+p_st <- replace_na(p_st,list(FGp = 0, x3Pp = 0, x2Pp = 0, eFGp = 0, FTp = 0 ))
+sum(is.na(p_st))
+
+#check missing values
+sum(is.na(p_sal))
+sum(is.na(p_st))
+sum(is.na(payroll))
+sum(is.na(t_st1))
+sum(is.na(t_st2))
+
+#4- Deal duplications in player statistics
+# Find the players who played in mutliple teams in one season (p_st)
+p_st <-p_st %>%
+  group_by(player_name, Pos) %>% 
+  summarise_each(funs(sum), 3:27)
 
 # write new dataframes into csv.
 
@@ -51,42 +103,8 @@ write_csv(p_st,"data/processed/1_2018-19_nba_player_statistics.csv")
 write_csv(t_st1,"data/processed/1_2018-19_nba_team_statistics_1.csv")
 write_csv(t_st2,"data/processed/1_2018-19_nba_team_statistics_2.csv")
 
-# find the duplications in player statistics (p_st)
-p_st_d <- p_st %>% 
-  group_by(player_name, Age) %>%  
-  mutate(index = n()) %>%
-  filter(index > 1) %>%
-  ungroup()
+#Explotry Data
 
-#exploring data
-
-# filter the players' information of Chicago Bulls
-p_CHI <- filter(p_st, Tm == "CHI")
-p_CHI <- p_CHI%>%
-  inner_join(x= p_CHI, y = p_sal,
-             by = "player_name")%>%
-  select(-c(player_id)) %>%
-  arrange(salary)
-write_csv(p_CHI,"data/processed/AllInfor_CHI_players.csv")
-
-# CHI players salaries
-p_CHI_sal <- select(p_CHI, player_name, Tm, Pos, salary)
-
-ggplot(data = p_CHI_sal,
-         aes(x = reorder(player_name, salary, FUN = median), y = salary/10000, 
-             fill = player_name ))+
-  geom_bar(stat ='identity')+
-  geom_hline(yintercept = (median(p_CHI_sal$salary))/10000,colour = "red", linetype = "dashed")+
-  theme(axis.text.x = element_text(angle = 45))+
-  scale_y_continuous(limits = c(0, 2700),
-                     breaks = seq(0,2700, by = 100))
-
-
-
-p_sal_c<- p_sal %>%
-  mutate(sal_mean = mean(salary),
-         sal_median = median(salary),
-         sal_sd = sd(salary))
 
 
 
