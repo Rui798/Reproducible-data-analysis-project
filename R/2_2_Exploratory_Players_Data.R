ef860afe8e8b85712c4f6cef950b90ae13ad4eb1
“RUI” <278350644@qq.com>
2020-05-28 14:44:33 +1000

    exploratory players data

--- R/2_2_Exploratory_Players_Data.R
+++ R/2_2_Exploratory_Players_Data.R
@@ -14,23 +14,133 @@ player <- player%>%
          BLK_Per_G = BLK / G,
          TOV_Per_G = TOV / G,) %>%
   mutate_if(is.numeric, round, digits = 3) %>%
-  select(-c(ORB:PTS)) %>%
-  select(-c(FG,FGA,x3P,x3PA,x2P,x2PA,FT,FTA))
+  select(-c(ORB:PTS)) 
 
-# Observe the relationship between Effective Field Goal Percentage and Points Per Game
+# 1-2 Filter players with low Minutes Played than average
 
-player%>% 
-  ggplot(aes(eFGp, PTS_Per_G)) +
+player1 <- player%>%
+  mutate(MP_z = (MP - mean(MP)) / sd(MP), 
+         MP_category = if_else(condition = MP_z < 0,true = "L", false = "H"))%>%
+  filter(MP_category == "H") 
+
+##Since we want to choose elite athletes (staring players), 
+##and higher Minutes Played is the way to indicate that the athletes have experiences and good performances. 
+##Therefore,the first step is to filter the players with low Minutes Played than average (2018-19 season).
+
+
+# 2 Observe the relationship and Pearson coefficient
+##2-1 Effective Field Goal Percentage and Points Per Game
+
+player1%>% 
+  ggplot(aes(PTS_Per_G, eFGp)) +
   geom_point() +
   geom_smooth(method = "lm") 
 
-cor(player$eFGp, player$PTS_Per_G, method = "pearson")
-#The Pearson coefficient is 0.285, 
-##so there is a weak positive correlation between Effective Field Goal Percentage and Points Per Game.
+cor(player1$eFGp, player1$PTS_Per_G, method = "pearson")
 
+#The Pearson coefficient is 0.045, 
+##there is barely no relationship between Effective Field Goal Percentage and Points Per Game.
 
 player1 %>% group_by(Pos) %>%
-  summarise(mn = mean(x2Pp, na.rm = TRUE), 
-            sd = sd(x2Pp, na.rm = TRUE), 
+  summarise(mn = mean(eFGp, na.rm = TRUE), 
+            sd = sd(eFGp, na.rm = TRUE), 
             count = n()) 
+#Players have not much difference in Effective Field Goal Percentage, 
+##and the centers' eFGp is higher than other positions.
+
+# 2-2 The relationship between Points Per Game and Total Rebounds Per Game.
+
+player1%>% 
+  group_by(Pos)%>%
+  ggplot(aes( PTS_Per_G, TRB_Per_G,color = Pos, shape = Pos)) +
+  geom_point() +
+  geom_smooth(method = "lm") 
+
+cor(player1$TRB_Per_G, player1$PTS_Per_G, method = "pearson")
+
+player1 %>% group_by(Pos) %>%
+  summarise(mn = mean(TRB_Per_G, na.rm = TRUE), 
+            sd = sd(TRB_Per_G, na.rm = TRUE), 
+            count = n()) 
+#Overall, the more rebounds per game, the higher the Points Per Game .
+#The Pearson coefficient is 0.448, indicating that the number of rebounds moderately impacts the points.
+#Most of rebounds are handled by Centers and Power Forwards.
+
+# 2-3 The relationship between Points Per Game and Assists Per Game.
+
+player1%>% 
+  group_by(Pos)%>%
+  ggplot(aes( PTS_Per_G, AST_Per_G,color = Pos, shape = Pos)) +
+  geom_point() +
+  geom_smooth(method = "lm") 
+
+cor(player1$PTS_Per_G, player1$AST_Per_G, method = "pearson")
+
+player1 %>% group_by(Pos) %>%
+  summarise(mn = mean(AST_Per_G, na.rm = TRUE), 
+            sd = sd(AST_Per_G, na.rm = TRUE), 
+            count = n()) 
+#Overall, the more Assists per game, the higher the Points Per Game .
+#The Pearson coefficient is 0.561, indicating that the number of Assists has significant impact on points.
+#Most assists are given by Point Guards.
+
+# 2-4 The relationship between Points Per Game and Steals Per Game.
+
+player1%>% 
+  group_by(Pos)%>%
+  ggplot(aes( PTS_Per_G, STL_Per_G,color = Pos, shape = Pos)) +
+  geom_point() +
+  geom_smooth(method = "lm") 
+
+cor(player1$STL_Per_G, player1$PTS_Per_G, method = "pearson")
+
+player %>% group_by(Pos) %>%
+  summarise(mn = mean(STL_Per_G, na.rm = TRUE), 
+            sd = sd(STL_Per_G, na.rm = TRUE), 
+            count = n()) 
+#Overall, the more Steals per game, the higher the Points Per Game .
+#The Pearson coefficient is 0.457, indicating that the number of Steals moderately impacts the points.
+#Most steals come from PG. 
+#From the standard deviation it can be seen that the difference in steal levels between players is large.
+
+# 2-5 The relationship between Points Per Game and Blocks Per Game.
+
+player1%>% 
+  group_by(Pos)%>%
+  ggplot(aes( PTS_Per_G, BLK_Per_G,color = Pos, shape = Pos)) +
+  geom_point() +
+  geom_smooth(method = "lm") 
+
+cor(player1$PTS_Per_G, player1$BLK_Per_G, method = "pearson")
+
+player1 %>% group_by(Pos) %>%
+  summarise(mn = mean(BLK_Per_G, na.rm = TRUE), 
+            sd = sd(BLK_Per_G, na.rm = TRUE), 
+            count = n()) 
+#Overall, the more Blocks per game, the higher the Points Per Game .
+#The Pearson coefficient is 0.193, indicating that the number of Blocks weakly impacts the points.
+#Obviously, most Blocks come from centers. 
+
+# 2-6 The relationship between Points Per Game and Free Throw Percentage.
+
+player1%>% 
+  group_by(Pos)%>%
+  ggplot(aes( PTS_Per_G,FTp,color = Pos, shape = Pos)) +
+  geom_point() +
+  geom_smooth(method = "lm") 
+
+cor(player1$PTS_Per_G, player1$FTp, method = "pearson")
+
+player1 %>% group_by(Pos) %>%
+  summarise(mn = mean(FTp, na.rm = TRUE), 
+            sd = sd(FTp, na.rm = TRUE), 
+            count = n()) 
+#Overall, the more Free Throw Percentage, the higher the Points Per Game .
+#The Pearson coefficient is 0.272, indicating that the Free Throw Percentage has weak impact on points.
+#The players' free throws are close to each other, their are all around 75%.   
+
+#3 Export new tables
+
+write_csv(x = player1, path = "data/processed/Player_Candidate.csv") 
+#To facilitate modeling and regression
 
