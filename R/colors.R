3e7f826254b2ef5e692dbefb1d4c458286422164
“RUI” <278350644@qq.com>
2020-05-31 10:05:07 +1000

    change some errors in .RMD

--- R/colors.R
+++
@@ -1,5 +0,0 @@
-team_colours <- 
-  
-  scale_fill_manual(values = c("dodgerblue4", "midnightblue", "steelblue3", "tan1", "black", "firebrick1", "mediumblue", "royalblue4", "black", "black", "black", "red1", "navyblue", "maroon", "black", "royalblue2", "red", "lightskyblue")) +
-  
-  scale_colour_manual(values = c("steelblue1", "gold", "steelblue4", "white", "gold", "black", "black", "gold", "yellow", "orange", "firebrick1", "white", "red", "midnightblue", "white", "white", "white", "midnightblue"))
